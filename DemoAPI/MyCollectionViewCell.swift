39f90704f2d75301dd525136af13344760d38bea
hamim <hamim4389@gmail.com>
2023-04-26 18:28:09 +0600

    Initial Commit

---
+++ DemoAPI/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  DemoAPI
+//
+//  Created by Hasibur Rahman on 26/4/23.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var myImageView: UIImageView!
+    
+}
---
+++ DemoAPI/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  DemoAPI
+//
+//  Created by Hasibur Rahman on 26/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+   
+    var myImages = [UIImage]()
+
+    @IBOutlet var myCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 1...10{
+            let img = UIImage(named: "demo.png") ?? UIImage()
+            myImages.append(img)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        myImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MyCollectionViewCell
+        else{
+            return UICollectionViewCell()
+        }
+        cell.myImageView.image = myImages[indexPath.row]
+        cell.myImageView.layer.cornerRadius = 10
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.size.width/3 - 3, height: collectionView.frame.size.height/5 - 3)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        3
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        3
+    }
+
+}
+
